f9ccb769f66067d21fb9435578efff98c0a858e9
Alex <1321312087@qq.com>
2021-10-15 21:58:31 +0800

    bug fixes

--- SLE_Iteration.py
+++ SLE_Iteration.py
@@ -3,13 +3,12 @@ class Iteration(object):
     def __init__(self, augMatrix, xList=[]):
         self._augMatrix = augMatrix
         self._len = len(self._augMatrix)
-        self._xIndex = []
         self._xList = self._initXList(xList)
 
     def jacobiIteration(self, num=100):
         count = 0
         deltaList = 1
-        while deltaList < 0.001:
+        while deltaList > 0.00001:
             deltaList = 0
             if count == num:
                 self._xList
@@ -27,12 +26,12 @@ class Iteration(object):
                 temp = self._xList[i]
                 self._xList[i] = (self._augMatrix[i][-1] - tempSum) / \
                     self._augMatrix[i][i]
-                deltaList += (self._xList - temp)**2
+                deltaList += (self._xList[i] - temp)**2
             deltaList = pow(deltaList, 0.5)
         return self._xList
 
     def _initXList(self, xList):
         if len(xList) == 0:
-            return [0 for i in range(self.__len)]
+            return [0 for i in range(self._len)]
         else:
             return xList
