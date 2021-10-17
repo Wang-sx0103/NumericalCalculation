ccd4f87833b8c9a388ba76eac2dc421c9f663c0e
Alex <1321312087@qq.com>
2021-10-17 19:50:44 +0800

    add Gauss-Seidel iteration function

--- SLE_Iteration.py
+++ SLE_Iteration.py
@@ -30,6 +30,31 @@ class Iteration(object):
             deltaList = pow(deltaList, 0.5)
         return self._xList
 
+    def GaussSeidel(self, num=100):
+        count = 0
+        deltaList = 1
+        while deltaList > 0.00001:
+            deltaList = 0
+            if count == num:
+                self._xList
+                break
+            else:
+                count += 1
+            for i in range(self._len):
+                tempSum = 0
+                temp = 0
+                for j in range(self._len):
+                    if i != j:
+                        tempSum += self._augMatrix[i][j] * self._xList[j]
+                    else:
+                        continue
+                temp = self._xList[i]
+                self._xList[i] = (self._augMatrix[i][-1] - tempSum) / \
+                    self._augMatrix[i][i]
+                deltaList += (self._xList[i] - temp)**2
+            deltaList = pow(deltaList, 0.5)
+        return self._xList
+
     def _initXList(self, xList):
         if len(xList) == 0:
             return [0 for i in range(self._len)]
