df50e764ef069fbb3784830b2f48ae32cf2964b7
Alex <1321312087@qq.com>
2021-10-28 21:41:45 +0800

    Add normalized power method

---
+++ CNum/Power.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import lib.Init as init
+import lib.MatCal as mc
+# import lib.Matrix as mat
+
+
+class Power(object):
+    def __init__(self, Matrix: list = [], xList: list = []):
+        self._matrix = Matrix
+        self._len = len(Matrix)
+        self._xList = mc.vectorToMat(init.initList(xList))
+
+    def setMatrix(self, Matrix: list):
+        self._matrix = Matrix
+
+    def getMatrix(self):
+        return self.Matrix
+
+    def setInitEigenvectors(self, xList: list):
+        self._xList = mc.vectorToMat(init.initList(xList))
+
+    def getEigenvectors(self):
+        return self._xList
+
+    # 规范化幂法
+    def NorPower(self, num=100, delta=0.000001):
+        count = 0
+        deltaNum = 1
+        lambda0 = 0
+        mu = 0
+        while deltaNum > delta:
+            if count == num:
+                break
+            deltaNum = 0
+            tempY = mc.matDivNum(self._xList, mc.absMax(self._xList, 1))
+            self._xList = mc.matMul(self._matrix, tempY)
+            lambda0 = mc.absMax(self._xList, 1)
+            deltaNum = abs(lambda0-mu)
+            mu = lambda0
+            count += 1
+        return lambda0
