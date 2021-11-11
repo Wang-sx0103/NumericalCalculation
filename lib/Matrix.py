99202f511e9574bf38e754f753435cd554d9b40e
Alex <1321312087@qq.com>
2021-11-11 20:00:20 +0800

    A data type

--- lib/Matrix.py
+++ lib/Matrix.py
@@ -5,6 +5,7 @@ class Matrix(object):
     def __init__(self, row: int, col: int = 1):
         self._row = row
         self._col = col
+        self._itera = 0
         self._array: list = [[0]*col for _ in range(row)]
 
     def __setitem__(self, key, value: float):
@@ -13,7 +14,7 @@ class Matrix(object):
         except IndexError:
             print("IndexError: Matrix assignment index out of range")
 
-    def __getitem__(self, key):
+    def __getitem__(self, key) -> float:
         try:
             return self._array[key[0]][key[1]]
         except IndexError:
@@ -23,31 +24,69 @@ class Matrix(object):
         return (self._row, self._row)
 
     def __repr__(self) -> str:
-        return '%r' % self._array
+        rep = "size:" + str(self._row) + chr(215) + str(self._row) + "\n"
+        for i in range(self._row):
+            if i == self._row - 1:
+                rep += str(self._array[i])
+            else:
+                rep += str(self._array[i]) + "\n"
+        return rep
 
-    def __add__(self, num: float) -> list:
-        self._returnArray: list = [[0]*self._col for _ in range(self._row)]
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    def __add__(self, num: float) -> None:
         for i in range(self._row):
             for j in range(self._col):
-                self._returnArray[i][j] = self._array[i][j]+num
-        return self._returnArray
+                self._array[i][j] = self._array[i][j]+num
 
-    def __sub__(self, num: float) -> list:
-        self._returnArray: list = [[0]*self._col for _ in range(self._row)]
+    def __sub__(self, num: float) -> None:
         for i in range(self._row):
             for j in range(self._col):
-                self._returnArray[i][j] = self._array[i][j]-num
-        return self._returnArray
+                self._array[i][j] = self._array[i][j]-num
 
-    def __mul__(self, num: float) -> list:
-        self._returnArray: list = [[0]*self._col for _ in range(self._row)]
+    def __mul__(self, num: float) -> None:
         for i in range(self._row):
             for j in range(self._col):
-                self._returnArray[i][j] = self._array[i][j]*num
-        return self._returnArray
+                self._array[i][j] = self._array[i][j]*num
 
-    def __truediv__(self, num: float) -> list:
+    def __truediv__(self, num: float) -> None:
         for i in range(self._row):
             for j in range(self._col):
-                self._returnArray[i][j] = self._array[i][j]/num
-        return self._returnArray
+                self._array[i][j] = self._array[i][j]/num
+
+    def __iter__(self) -> int:
+        return self
+
+    def __next__(self) -> float:
+        if self._itera < self._row*self._col:
+            row = self._itera//self._col
+            col = self._itera % self._col
+            self._itera += 1
+            return self._array[row][col]
+        else:
+            raise StopIteration
+
+    def size(self) -> tuple:
+        return self.__len__()
+
+    def tran(self) -> None:
+        for i in range(self._row):
+            for j in range(self._col):
+                if i > j:
+                    self._array[j][i], self._array[i][j] = \
+                        self._array[i][j], self._array[j][i]
+
+    def Max(self, flag: int = 0) -> float:
+        max = 0
+        for i in range(self._row):
+            for j in range(self._col):
+                if max < abs(self._array[i][j]):
+                    max = abs(self._array[i][j])
+                    indexI = i
+                    indexJ = j
+
+        if flag == 0:
+            return self._array[indexI][indexJ]
+        else:
+            return max
