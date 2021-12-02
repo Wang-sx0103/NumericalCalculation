ca2bcfe3fcf9a21df27aa57c4aaaa3a4b39898f6
Alex <1321312087@qq.com>
2021-12-02 14:14:36 +0800

    Add notes

--- CNum/FuncAppro.py
+++ CNum/FuncAppro.py
@@ -1,47 +1,93 @@
 # -*- coding: utf-8 -*-
+'''
+Interpolation
+    This class contains a methods in order to construct an n-order polynomial.
+Function list
+    PolyFitting: Fitting an n-order polynomial.
+Others:
+
+'''
 import lib.Init as init
 from CNum.Elimination import Elimination as et
 
 
-class Approximation (object):
+class FuncAppro (object):
+    '''
+    This class contains a methods in order to construct an n-order polynomial.
+    '''
     def __init__(self,
                  xList: list = [],
                  yList: list = []) -> None:
+        '''
+        xList: You need to provide a set of x-points.
+        If you do not provide the vector here,
+        you must provide it at the function called
+        setListX().\n
+        yList: You need to provide a set of y-points.
+        If you do not provide the vector here,
+        you must provide it at the function called
+        setListY().\n
+        The length of "xList" must be equal to "yList".
+        '''
         self._xList = xList
         self._yList = yList
         self._len = len(xList)
 
     def setListX(self, xList) -> None:
+        '''
+        xList: You can provide a set of x-points.
+        '''
         self._xList = xList
 
     def getListX(self) -> list:
+        '''
+        return: We will return a list contain all x-points.
+        '''
         return self._xList
 
     def setListY(self, yList) -> None:
+        '''
+        yList: You can provide a set of y-points.
+        '''
         self._yList = yList
 
     def getListY(self) -> list:
+        '''
+        return: We will return a list contain all y-points.
+        '''
         return self._yList
 
     def geAugMat(self) -> list:
+        '''
+        return: We will return the coefficient matrix of the normal equations.
+        Its size: (order, order+1)
+        '''
         return self._augMat
 
-    def PolyFitting(self, n: int) -> list:
-        self._conCoeffMat(n)
-        alist = et(self._augMat).completeEliminate()
-        return alist
+    def PolyFitting(self, order: int) -> list:
+        '''
+        order: It represents the order of the fitting polynomial,
+        you must provide an integer less than the number of coordinates.\n
+        return: We will return a list with a length of "order"+1.\n
+        It represents the coefficients of the fitting polynomial
+        from low to high.
+        '''
+        self._augMat = self._conCoeffMat(order)
+        coefficientList = et(self._augMat).completeEliminate()
+        return coefficientList
 
     def _conCoeffMat(self, n: int) -> list:
-        self._augMat = init.Matrix(n+1, n+2)
+        resultMat = init.Matrix(n+1, n+2)
         for i in range(n+1):
             for j in range(i, n+1):
                 for k in range(self._len):
-                    self._augMat[i][j] += self._xList[k]**(j+i)
+                    resultMat[i][j] += self._xList[k]**(j+i)
                     if j == n:
                         if i == 0:
-                            self._augMat[i][-1] += self._yList[k]
+                            resultMat[i][-1] += self._yList[k]
                         else:
-                            self._augMat[i][-1] += self._yList[k] * \
+                            resultMat[i][-1] += self._yList[k] * \
                                 self._xList[k]**(i)
-            self._augMat[j][i] = self._augMat[i][j]
-        self._augMat[0][0] = self._len
+            resultMat[j][i] = resultMat[i][j]
+        resultMat[0][0] = self._len
+        return resultMat
