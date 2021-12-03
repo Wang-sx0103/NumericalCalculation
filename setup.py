03783c1f910d8d59d596321c77bb7f9d58ebcb72
Alex <1321312087@qq.com>
2021-12-03 21:54:10 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+# dev_requires = [
+#     'pytest',
+#     'pytest-cov',
+# ]
+
+
+setup(
+    author='Shangxing Wang',
+    author_email='AlexWang0103@outlook.com',
+    name='CNum',
+    version='0.6.5',
+    description='A library related to common numerical calculations.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Mathematics"
+    ],
+    url='https://github.com//Wang-sx0103//NumericalCalculation',
+    packages=find_packages(),
+    python_requires='>=3',
+    include_package_data=False,
+    zip_safe=False,
+    # extras_require={
+    #   'dev': dev_requires,
+    # },
+)
